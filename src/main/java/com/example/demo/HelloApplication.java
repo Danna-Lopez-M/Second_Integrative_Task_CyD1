68bfa6ed9761e459d73c63894d18907f3b0ff798
Juan Camilo Corrales Osvath <juancamilocorra@gmail.com>
2023-11-22 22:31:25 -0500

    added vertices view on screen

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -5,6 +5,7 @@ import com.example.demo.dataStructures.AdjacencyMatrix;
 import com.example.demo.dataStructures.IDataStructures.IGraph;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
@@ -12,6 +13,7 @@ import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 import javafx.scene.Scene;
@@ -20,53 +22,58 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class HelloApplication extends Application {
+    private static final double NODE_SIZE = 20.0;
     @Override
     public void start(Stage stage) throws IOException {
         // Instancia de grafo utilizando lista de adyacencia
-        IGraph<String> adjacencyListGraph = new AdjacencyList<>();
+        //IGraph<String> graph = new AdjacencyList<>();
+        IGraph<String> graph = new AdjacencyMatrix<>();
 
         // Agregar 50 vértices
         for (int i = 1; i <= 50; i++) {
-            adjacencyListGraph.addVertex("V" + i);
+            graph.addVertex("V" + i);
         }
 
 // Conectar todos los vértices entre sí
         for (int i = 1; i <= 50; i++) {
             for (int j = i + 1; j <= 50; j++) {
                 int weight = (int) (Math.random() * 10) + 1; // Peso aleatorio entre 1 y 10
-                adjacencyListGraph.addEdge("V" + i, "V" + j, weight);
-                adjacencyListGraph.addEdge("V" + j, "V" + i, weight); // Asegurar conexión bidireccional
+                graph.addEdge("V" + i, "V" + j, weight);
+                graph.addEdge("V" + j, "V" + i, weight); // Asegurar conexión bidireccional
             }
         }
         // Aplicar el algoritmo de Prim
-        Map<String, String> primResult = adjacencyListGraph.primMST();
+        Map<String, String> primResult = graph.primMST();
 
-        // Crear una interfaz gráfica para visualizar el árbol de recubrimiento mínimo
-        Pane root = new Pane();
+        // Crear una escena
+        Group root = new Group();
+        Scene scene = new Scene(root, 800, 600);
 
-        // Crear RadioButtons para cada vértice
-        ToggleGroup toggleGroup = new ToggleGroup();
-        for (String vertex : adjacencyListGraph.getVertices()) {
+// Distribuir los RadioButton y conectarlos según el árbol de recubrimiento mínimo
+        Map<String, RadioButton> nodeMap = new HashMap<>();
+        for (String vertex : graph.getVertices()) {
             RadioButton radioButton = new RadioButton(vertex);
-            radioButton.setToggleGroup(toggleGroup);
+            nodeMap.put(vertex, radioButton);
             root.getChildren().add(radioButton);
         }
 
-        // Dibujar las aristas del árbol de recubrimiento mínimo
-        for (Map.Entry<String, String> entry : primResult.entrySet()) {
-            String sourceVertex = entry.getKey();
-            String destinationVertex = entry.getValue();
+        for (String vertex : graph.getVertices()) {
+            RadioButton radioButton = nodeMap.get(vertex);
 
-            // Encuentra los RadioButtons correspondientes a los vértices de la arista
-            RadioButton sourceButton = findButton(root, sourceVertex);
-            RadioButton destinationButton = findButton(root, destinationVertex);
+            String parentVertex = primResult.get(vertex);
+            if (parentVertex != null) {
+                RadioButton parentRadioButton = nodeMap.get(parentVertex);
 
-            // Dibujar la arista entre los RadioButtons
-            // Aquí podrías dibujar una línea, unir los vértices visualmente, etc.
+                // Posicionar el RadioButton actual al lado del padre y evitar superposiciones
+                positionNodeAvoidOverlap(radioButton, parentRadioButton);
+            } else {
+                // Si no tiene padre, colócalo en una posición inicial y evita superposiciones
+                positionNodeAvoidOverlap(radioButton, null);
+            }
         }
 
-        Scene scene = new Scene(root,800, 600);
-        stage.setTitle("Labyrinth Visualization");
+        // Configurar y mostrar la ventana
+        stage.setTitle("Graph Visualization");
         stage.setScene(scene);
         stage.show();
     }
@@ -76,6 +83,24 @@ public class HelloApplication extends Application {
                 .filter(node -> node instanceof RadioButton && ((RadioButton) node).getText().equals(vertex))
                 .findFirst()
                 .orElse(null);
+    }
+    private void positionNodeAvoidOverlap(RadioButton node, RadioButton parent) {
+        double x = Math.random() * 800;
+        double y = Math.random() * 600;
+
+        if (parent != null) {
+            double parentX = parent.getLayoutX();
+            double parentY = parent.getLayoutY();
+
+            // Evitar superposición
+            while (Math.abs(x - parentX) < NODE_SIZE || Math.abs(y - parentY) < NODE_SIZE) {
+                x = Math.random() * 800;
+                y = Math.random() * 600;
+            }
+        }
+
+        node.setLayoutX(x);
+        node.setLayoutY(y);
     }
     public static void main(String[] args) {
         launch(args);
